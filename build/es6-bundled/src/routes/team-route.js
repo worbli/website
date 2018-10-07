0fac3ba6449577c0f56c10a2cd147c7fffd2e066
Will Hill <email.will.in.china@gmail.com>
2018-10-07 08:20:01 +0800

    build

---
+++ build/es6-bundled/src/routes/team-route.js
@@ -0,0 +1,14 @@
+define(["../worbli-portal.js"],function(_worbliPortal){"use strict";class TeamRoute extends _worbliPortal.PolymerElement{static get template(){return _worbliPortal.html`
+      <style include="shared-styles">
+        :host {
+          display: block;
+        }
+      </style>
+
+      <div class="card">
+        <h1>Team Route</h1>
+        <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
+        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
+      </div>
+      <worbli-footer name="footer"></worbli-footer>
+    `}}window.customElements.define("team-route",TeamRoute)});
---
+++ build/esm-bundled/src/routes/about-route.js
@@ -0,0 +1,13 @@
+import{PolymerElement,html}from"../worbli-portal.js";class AboutRoute extends PolymerElement{static get template(){return html`
+      <style include="shared-styles">
+        :host {
+          display: block;
+        }
+      </style>
+      <div class="card">
+        <h1>About Route</h1>
+        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
+        <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
+      </div>
+      <worbli-footer name="footer"></worbli-footer>
+    `}}window.customElements.define("about-route",AboutRoute);
