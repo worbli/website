4ffb34909120fcfeef45a292e706b59b55306d94
Will Hill <email.will.in.china@gmail.com>
2018-10-23 18:05:22 +0800

    formated terms and privacy

--- src/routes/terms-route.js
+++ src/routes/terms-route.js
@@ -1,7 +1,9 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '../css/shared-styles.js';
 import '../components/worbli-footer.js';
-
+import '../components/side-bar/worbli-telegram.js';
+import '../components/side-bar/worbli-whitepaper.js';
+import '../components/side-bar/worbli-support.js';
 
 class TermsRoute extends PolymerElement {
   static get template() {
@@ -13,6 +15,8 @@ class TermsRoute extends PolymerElement {
         iframe {
           margin-top: 50px;
           margin-bottom: 50px;
+          width: 100%;
+          height: 8200px;
         }
         .split {
           display: flex;
@@ -78,47 +82,14 @@ class TermsRoute extends PolymerElement {
       <div class="split">
         <div class="main">
           <h1>Terms & Conditions</h1>
-          
-<h2>1. Terms</h2>
-By accessing this Website, accessible from WORBLI.io, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.
-
-<h2>2. Use License</h2>
-Permission is granted to temporarily download one copy of the materials on WORBLI's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
-
-modify or copy the materials;
-use the materials for any commercial purpose or for any public display;
-attempt to reverse engineer any software contained on WORBLI's Website;
-remove any copyright or other proprietary notations from the materials; or
-transferring the materials to another person or "mirror" the materials on any other server.
-This will let WORBLI to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.
-
-<h2>3. Disclaimer</h2>
-All the materials on WORBLI’s Website are provided "as is". WORBLI makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, WORBLI does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
-
-<h2>4. Limitations</h2>
-WORBLI or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on WORBLI’s Website, even if WORBLI or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.
-
-<h2>5. Revisions and Errata</h2>
-The materials appearing on WORBLI’s Website may include technical, typographical, or photographic errors. WORBLI will not promise that any of the materials in this Website are accurate, complete, or current. WORBLI may change the materials contained on its Website at any time without notice. WORBLI does not make any commitment to update the materials.
-
-<h2>6. Links</h2>
-WORBLI has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by WORBLI of the site. The use of any linked website is at the user’s own risk.
-
-<h2>7. Site Terms of Use Modifications</h2>
-WORBLI may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
-
-<h2>8. Governing Law</h2>
-Any claim related to WORBLI's Website shall be governed by the laws of us without regards to its conflict of law provisions.
+          <iframe src="https://docs.google.com/document/d/e/2PACX-1vRHsvfVKf9guVGUOHZf27Y3TJ3vnK-_vosVGabk4FX6N5qu4XTVe7vLoUy6x5BvVWkE1AkKmRZsSlKM/pub?embedded=true"></iframe>
 
 
         </div>
         <div class="side">
-        <div class="container">
-            <div class="title">White Paper</div>
-        </div>
-        <div class="container">
-            <div class="title">Our Partners</div>
-        </div>
+          <worbli-support></worbli-support>
+          <worbli-whitepaper></worbli-whitepaper>
+          <worbli-telegram></worbli-telegram>
         </div>
       </div>
       <worbli-footer name="footer"></worbli-footer>
