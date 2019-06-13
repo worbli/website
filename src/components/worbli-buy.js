5769877c1a6742e39eb7e0b4ece65b4da93df000
Robert DeWilder <rob@worbli.io>
2019-06-13 11:03:01 -0400

    removed eosex from buy section

--- src/components/worbli-buy.js
+++ src/components/worbli-buy.js
@@ -59,7 +59,6 @@ class WorbliBuy extends PolymerElement {
             <div class="btn-container">
                 <a href="https://chaince.com/registrations/new?in_token=89a7a25f" target="_blank"><div class="yellow-button">CHAINCE</div></a>
                 <a href="https://www.chainrift.com/?R=UEQAo" target="_blank"><div class="yellow-button">CHAINRIFT</div></a>
-                <a href="https://eosex.com/#/register?code=v16JyGsK" target="_blank"><div class="yellow-button">EOSEX</div></a>
             </div>
         </div>
     `;
